ce43dbd4eb37dabbe840a30bdcbe837567948310
JPU322 <jpuntivero@gmail.com>
2014-02-03 23:58:43 -0600

    Project 01 Comments

--- source/project01.c
+++ source/project01.c
@@ -4,25 +4,36 @@
  *
  * Created on February 3, 2014, 4:44 PM
  */
-m
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
+    // Create the FLOAT variables for the HIGHEST and INPUT values
     float high, inp;
+    // Create the int variable for STOP | 0 - False | 1 - True
     int stop = 0;
+
+    // While STOP is [FALSE | 0]
     while (stop == 0) {
+        // Asks and gets the input number
         printf("Enter a number: ");
         scanf("%f", &inp);
+
+        // Specific tasks depending on the input
         if (inp < 0)
+            // If it is negative
             printf("Not a valid number\n");
         else if (inp == 0)
+            // If the input is 0, stop the loop
             stop = 1;
         else if (inp > high)
+            // If the input is highest, set the highest to the input
             high = inp;
     }
+    // Display the highest numbers
     printf("Highest Number: %.2F\n", high);
-    
+
     return (EXIT_SUCCESS);
 }
 
